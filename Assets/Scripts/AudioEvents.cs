6fd6d59dd773875263bec39c12302caa969aec86
Damian Niedziółka <95479392+Karmelica@users.noreply.github.com>
2025-06-26 10:22:00 +0200

    zaliczenie

--- Assets/Scripts/AudioEvents.cs
+++ Assets/Scripts/AudioEvents.cs
@@ -7,18 +7,30 @@ public class AudioEvents : MonoBehaviour
     
     [Header("Snapshots")]
     public EventReference insideRoomSnapshot;
+    public EventReference globalMuteSnapshot;
+    public EventReference musicMuteSnapshot;
+    public EventReference ambientMuteSnapshot;
+    public EventReference outsideMuteSnapshot;
+    public EventReference lowHealthSnapshot;
+    
+    [Header("Health")]
+    public EventReference lowHealth;
     
     [Header("Player Sounds")]
     public EventReference footSteps;
     public EventReference jump;
 
-    [Header("Interaction")] public EventReference doorOpen;
+    [Header("Interaction")]
+    public EventReference doorOpen;
     
     [Header("Ambient Sounds")]
     public EventReference ambientForest;
     public EventReference ambientFire;
     
-    [Header("Music")] public EventReference tavernMusic;
+    [Header("Music")]
+    public EventReference tavernMusic;
+
+    public EventReference tavernAmbient;
 
     private void Awake()
     {
--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -7,6 +7,11 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
     public EventInstance insideRoomSnapshotInstance;
+    public EventInstance GlobalMuteInstance;
+    public EventInstance MusicMuteInstance;
+    public EventInstance AmbientMuteInstance;
+    public EventInstance OutsideMuteInstance;
+    public EventInstance LowHealthSnapshotInstance;
     
     private void Awake()
     {
@@ -40,8 +45,43 @@ public class AudioManager : MonoBehaviour
         return eventInstance;
     }
 
+    protected virtual void MuteInput(EventInstance eventInstance, KeyCode keyCode)
+    {
+        if (Input.GetKeyDown(keyCode))
+        {
+            eventInstance.start();
+        }
+
+        if (Input.GetKeyUp(keyCode))
+        {
+            eventInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        }
+    }
+
+    private void MuteOnInput()
+    {
+        MuteInput(GlobalMuteInstance, KeyCode.U);
+        MuteInput(MusicMuteInstance, KeyCode.I);
+        MuteInput(AmbientMuteInstance, KeyCode.O);
+        MuteInput(OutsideMuteInstance, KeyCode.P);
+        MuteInput(LowHealthSnapshotInstance, KeyCode.K);
+    }
+
+    private void Update()
+    {
+        MuteOnInput();
+    }
+
     private void Start()
     {
+        var healthEvent = NewEventInstance(AudioEvents.instance.lowHealth);
+        healthEvent.start();
+        
         insideRoomSnapshotInstance = NewEventInstance(AudioEvents.instance.insideRoomSnapshot);
+        GlobalMuteInstance = NewEventInstance(AudioEvents.instance.globalMuteSnapshot);
+        MusicMuteInstance = NewEventInstance(AudioEvents.instance.musicMuteSnapshot);
+        AmbientMuteInstance = NewEventInstance(AudioEvents.instance.ambientMuteSnapshot);
+        OutsideMuteInstance = NewEventInstance(AudioEvents.instance.outsideMuteSnapshot);
+        LowHealthSnapshotInstance = NewEventInstance(AudioEvents.instance.lowHealthSnapshot);
     }
 }
